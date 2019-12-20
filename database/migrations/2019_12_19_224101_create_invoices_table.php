038f2c76f7d1e1ca3a98c856f27d6564a5472c6f
r.martinez1 <r.martinez@empresasim.com>
2019-12-19 20:54:18 -0400

    Make migrations

---
+++ database/migrations/2019_12_19_224101_create_invoices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('date');
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('payment_mode_id');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('payment_mode_id')->references('id')->on('payment_modes');
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
